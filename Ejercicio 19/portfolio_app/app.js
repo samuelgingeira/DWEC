4b26b39024f424641dbbb261b44eeea91b08ce8f
samuelgingeira <samuelmg66@educastur.es>
2026-05-14 17:17:46 +0200

    portfolio app final

--- Ejercicio 19/portfolio_app/app.js
+++ Ejercicio 19/portfolio_app/app.js
@@ -95,8 +95,10 @@ app.post("/login", (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log("Servidor en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Servidor funcionando");
 });
 
 function authMiddleware(req, res, next) {
